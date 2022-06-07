2c6ba49e04768fa23fe618f117336b201d4d6bb1
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-06-07 15:03:19 -0700

    get single entry via api

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require('express')
-const app = express()
-const PORT = 3001  
+const express = require('express');
+const app = express();
+const PORT = 3001;
 
 // Phonebook sample data
 let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
 
 // Routing
 // Root
 app.get('/', (req, res) => {
-    res.send(`
+  res.send(`
     <h1>Phonebook API</h1>
     <p>Thanks for visiting this API page. In order to interact with the API please see the endpoints below:</p>
     <ul>
@@ -37,25 +37,36 @@ app.get('/', (req, res) => {
             <h2>Entire phonebook</h2>
             <span>http://localhost:3001/api/persons</span>
         </li>
-    </ul>`)
-})
-
-// Get entire phonebook
-app.get('/api/persons', (req, res) => {
-    res.json(phonebook)
-})
+    </ul>`);
+});
 
 // Get info about the phonebook
 app.get('/info', (req, res) => {
-    const numOfEntries = phonebook.length
-    const now = new Date()
-    res.send(`
-    <p>Phonebook has info for ${numOfEntries} people</p>
-    <p>${now}</p>
-    `)
-})
+  const numOfEntries = phonebook.length;
+  const now = new Date();
+  res.send(`
+  <p>Phonebook has info for ${numOfEntries} people</p>
+  <p>${now}</p>
+  `);
+});
+
+// Get entire phonebook
+app.get('/api/persons', (req, res) => {
+  res.json(phonebook);
+});
+
+// Get info for a single person
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const entry = phonebook.find((item) => item.id === +id);
+  if (entry) {
+    res.json(entry);
+  } else {
+    res.status(404).end();
+  }
+});
 
 // Listen with server
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-})
+  console.log(`Server is running on port: ${PORT}`);
+});
